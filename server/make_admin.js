b3a117ad02fbaab294b86c1c2cc673336e8d3517
Yaron Serlin <yaron155@gmail.com>
2026-03-30 19:10:59 +0700

    change to logger

--- server/make_admin.js
+++ server/make_admin.js
@@ -6,32 +6,34 @@
 const mongoose = require('mongoose');
 const User = require('./src/models/User');
 const path = require('path');
+const logger = require('./src/utils/logger');
 require('dotenv').config({ path: path.join(__dirname, '.env') });
 
 const makeAdmin = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
-        console.log('MongoDB Connected');
+        logger.info('MongoDB Connected');
+
 
         const email = process.argv[2];
         if (!email) {
-            console.error('Please provide an email address as an argument.');
+            logger.error('Please provide an email address as an argument.');
             process.exit(1);
         }
-
+        logger.info(`Looking for user with email: ${email}`);
         const user = await User.findOne({ email });
         if (!user) {
-            console.error(`User with email ${email} not found.`);
+            logger.error(`User with email ${email} not found.`);
             process.exit(1);
         }
-
+        logger.info(`User found: ${user.email} - Current isAdmin: ${user.isAdmin}`);
         user.isAdmin = true;
         await user.save();
-        console.log(`User ${email} is now an Admin.`);
+        logger.info(`User ${email} is now an Admin.`);
 
         process.exit();
     } catch (err) {
-        console.error(err);
+        logger.error(err);
         process.exit(1);
     }
 };
